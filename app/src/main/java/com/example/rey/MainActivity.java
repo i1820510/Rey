3238698d8decbde5a7d8dd63ea10d298967ff3d6
i1820510 <i1820510@continental.edu.pe>
2019-12-14 16:02:00 +0100

    submaster commit2

--- app/src/main/java/com/example/rey/MainActivity.java
+++ app/src/main/java/com/example/rey/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.rey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View V){
+        Intent intento = new Intent(this, ListProducts.class);
+        startActivity(intento);
+    }
+
+    public void onClick1(View V){
+        Intent intento = new Intent(this, Login.class);
+        startActivity(intento);
+    }
+
+    public void onClick2(View V){
+        Intent intento = new Intent(this, mi_carta.class);
+        startActivity(intento);
     }
 }
