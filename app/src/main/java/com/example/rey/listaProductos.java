94dbc5d1588c98a25731e07e443e31c7cda47a5d
i1820510 <i1820510@continental.edu.pe>
2019-12-14 06:57:54 +0100

    cm0 dev.v0

---
+++ app/src/main/java/com/example/rey/listaProductos.java
@@ -0,0 +1,14 @@
+package com.example.rey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listaProductos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_productos);
+    }
+}
